d104c28c40dcd077aeceedd871cceeddfa31f116
Willianx19 <williansoares2015@outlook.com.br>
2025-05-26 18:59:00 +0000

    Desafio novato - Completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,38 +3,49 @@
 int main() {
 
 //Variaveis, arrays e matriz:
-int tabuleiro [10] [10];
 char letra[10] = {'A','B','C','D','E','F','G','I','J'};
 int i,j;
-
-printf("   ");
-printf("Batalha naval \n");
+int tabuleiro [10] [10];
 
 //Formando a tabela:
 for(int i = 0; i < 10; i++)
 {
-    for (int j = 0; i < 10; j++)
+    for(int j = 0; j < 10; j++)
     {
-    
+    tabuleiro [i][j]= 0;
     }
 }
 
+printf("     ");
+printf("Batalha naval \n");
+
 //Exibindo as letras no tabuleiro:
-printf("  ");
+printf("    ");
 for(int i = 0; i < 10; i++)
 {
 printf("%c ",letra [i]);
 }
 printf(" \n");
 
+//Exibindo o número 3 na tabela(Coluna):
+tabuleiro [4] [6] = 3;
+tabuleiro [5] [6] = 3;
+tabuleiro [6] [6] = 3;
+
+//Exibindo o número 3 na tabela(Coluna):
+tabuleiro [2] [4] = 3;
+tabuleiro [2] [5] = 3;
+tabuleiro [2] [6] = 3;
+
 //Exibindo o tabuleiro com os númerso na lateral:
 for(int i = 0; i < 10; i++)
 {
-printf("%d \n",i +1);
+printf("%d ",i +1);
     for(int j = 0; j < 10; j++)
     {
-printf("%d ",tabuleiro [i][j]);
+printf(" %d",tabuleiro [i][j]);
     }
+    printf(" \n");
 }
 
 
